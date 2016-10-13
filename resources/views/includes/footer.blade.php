b57d0e7fa15c23cf3bb963e138b5923d442379dd
Ed Crampin <edcrampin@gmail.com>
2016-10-13 11:24:16 +0100

    Added basic styling to logo

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="container">
         <div class="copy">
-            Copyright &copy; {{ date('Y') }} TheProductizer.
+            Copyright &copy; {{ date('Y') }} The Productizer.
         </div>
     </div>
 </div>
