1b995e60c0035d80bb7e519b8950a949b462ce1c
Ed Crampin <edcrampin@gmail.com>
2016-11-14 20:57:15 +0000

    StageController update

--- app/Http/Controllers/StageController.php
+++ app/Http/Controllers/StageController.php
@@ -128,7 +128,8 @@ class StageController extends Controller
                     ]
                 ]);
 
-                dd($result->getBody()->getContents());
+                $json_out = json_decode($result->getBody());
+                dd($json_out->class);
 
                 // TODO: make it go to guess mode with info
                 return redirect()->back();
