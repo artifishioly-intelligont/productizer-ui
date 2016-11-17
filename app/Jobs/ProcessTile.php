fd3bb54b90fef572e7ae83446d090e6c7d7f616a
Ed Crampin <edcrampin@gmail.com>
2016-11-17 14:32:46 +0000

    Pubnub fix

--- app/Jobs/ProcessTile.php
+++ app/Jobs/ProcessTile.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Tile;
 use Log;
-use Pubnub\Pubnub;
+use \Pubnub\Pubnub;
 
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
