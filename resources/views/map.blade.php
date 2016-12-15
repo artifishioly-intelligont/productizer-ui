e03f9226c476827a202b4b64925adc1087cd6fc7
Ed Crampin <edcrampin@gmail.com>
2016-12-15 19:28:53 +0000

    Removed unique marker colours for now

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -195,10 +195,10 @@ function rgbToHex(r, g, b) {
 
   @foreach($features as $key => $feature)
     mapMarkers["{{ $feature }}"] = [];
-    markerImages["{{ $feature }}"] = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + hslToRGB({{ 360 * ($key + 1) / count($features) }}, 1, 0.5),
+    /*markerImages["{{ $feature }}"] = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + hslToRGB({{ 360 * ($key + 1) / count($features) }}, 1, 0.5),
             new google.maps.Size(21, 34),
             new google.maps.Point(0,0),
-            new google.maps.Point(10, 34));
+            new google.maps.Point(10, 34));*/
   @endforeach
   @foreach($tiles as $tile)
     @if($tile->classification != null)
@@ -251,7 +251,7 @@ function rgbToHex(r, g, b) {
           position: centerLatLng,
           map: map,
           title: feature,
-          icon: markerImages[feature],
+          //icon: markerImages[feature],
         });
 
         var infowindow = new google.maps.InfoWindow({
