a1bedc4e80bebccc1fb3442a76babc3f3c46585c
Ed Crampin <edcrampin@gmail.com>
2017-01-26 12:31:22 +0000

    Test

--- app/Jobs/ProcessTile.php
+++ app/Jobs/ProcessTile.php
@@ -45,6 +45,7 @@ class ProcessTile implements ShouldQueue
         ]);
 
         $json_out = json_decode($result->getBody());
+        dd($json_out);
         Log::info($json_out->class);
         $this->tile->classification = $json_out->class;
         $this->tile->save();
