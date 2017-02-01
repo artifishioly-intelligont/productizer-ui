5ce3008caf32361a70e59ff9b5131be512e44000
Ed Crampin <edcrampin@gmail.com>
2017-02-01 18:27:32 +0000

    Test

--- app/Jobs/ProcessTile.php
+++ app/Jobs/ProcessTile.php
@@ -52,7 +52,6 @@ class ProcessTile implements ShouldQueue
             $retries--;
         }
         if($success == false) {
-            dd($json_out);
             throw new \Exception;
         }
         Log::info($json_out->class);
