ff1e1ec9c11f4a9cca8a48ba552ccd4366ceae85
Ed Crampin <edcrampin@gmail.com>
2017-03-03 18:53:41 +0000

    Added /clearsqs

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,30 @@ Route::get('/map/{id}', ['as' => 'stage1', 'uses' => 'StageController@showStage2
 Route::post('/map/{id}', ['as' => 'stage2_post', 'uses' => 'StageController@postStage2']);
 
 Route::get('/requeue/{id}', ['as' => 'requeue', 'uses' => 'StageController@requeue']);
+
+
+Route::get('/clearsqs', function() {
+
+      /*  'sqs' => [
+            'driver' => 'sqs',
+            'key' => 'AKIAISLQWKGFVT6ADGIA',
+            'secret' => '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',
+            'prefix' => 'https://sqs.eu-west-1.amazonaws.com/243684770939',
+            'queue' => 'Productizer',
+            'region' => 'eu-west-1',
+        ],*/
+
+    $client = Aws\Sqs\SqsClient::factory(array(
+                    'credentials' => array(
+                        'key'    => config('queue.connections.sqs.key'),
+                        'secret' => config('queue.connections.sqs.secret')
+                    ),
+                    'region' => config('queue.connections.sqs.region'),
+                    'version' => '2012-11-05'
+            ));
+    $client->purgeQueue(array(
+        // QueueUrl is required
+        'QueueUrl' => config('queue.connections.sqs.prefix').'/'.config('queue.connections.sqs.queue'),
+    ));
+    return redirect('/');
+});
